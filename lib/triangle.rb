0f53963f51c27a19919435e0a9bc9e9f1917778d
Marley Watson <marleykw17@gmail.com>
2023-06-09 10:08:38 -0400

    commit tree c70ff8badc772f9fc9e75837294c2b16650fc7e0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    sides = [a,b,c]
+    ineq = [a+b > c, a+c > b, b+c > a]
+
+    if sides.min <= 0 || ineq.include?(false)
+      raise TriangleError
+    end
+
+    if equilateral?
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif scalene?
+      :scalene
+    elsif isosceles?
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end #kind
+
+  def equilateral?
+    a == b && b == c
+  end
+  def isosceles?
+    (a == b && b != c) || (b == c && c != a) || (a == c && c != b)
+  end
+  def scalene?
+    a != b && a != c && b != c
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
 end
+
+  
+
+binding.pry
